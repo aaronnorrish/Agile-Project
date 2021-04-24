c6843a22d9719f209f909d7e0a6ae9c61f7b9c87
ge75ceb <ge75ceb@mytum.de>
2021-04-24 19:11:20 +0800

    add routing of pre-login webpages

--- learn_italian_flask/routes.py
+++ learn_italian_flask/routes.py
@@ -4,7 +4,13 @@ from learn_italian_flask import app
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('index.html', title="Learn Italian")
     # return render_template('dashboard.html', title="Dashboard", user={'name': '[placeholder]'})
-    # return render_template('index.html', title="Learn Italian")
-    # return render_template('log-in.html', title="Learn Italian - Log in")
+
+@app.route('/login')
+def login():
+    return render_template('log-in.html', title="Learn Italian - Log in")
+
+@app.route('/signup')
+def signup():
     return render_template('sign-up.html', title="Learn Italian - Sign up")
