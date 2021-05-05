c9ccef5d0b0cc5ec4780cd8f2aadc9e4b8b65365
ge75ceb <ge75ceb@mytum.de>
2021-05-05 12:49:17 +0800

    implement basic error logging and html pages

---
+++ learn_italian_flask/errors.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from learn_italian_flask import app, db
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html', title="404"), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('errors/500.html', title="500"), 500
