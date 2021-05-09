a769cd1e484ea6a38f0df9814c9c371b5123ece2
ge75ceb <ge75ceb@mytum.de>
2021-05-09 17:34:23 +0800

    add helper functions to retrieve user answers from form and to calculate score

--- learn_italian_flask/controllers.py
+++ learn_italian_flask/controllers.py
@@ -50,7 +50,6 @@ class DashboardController():
             modules.append("cards/_alphabet_card.html")
         elif current_user.get_next_module() == "numbers":
             modules.append("cards/_numbers_card.html")
-        print(current_user.get_progress())
         return render_template('dashboard.html', title="Dashboard", progress=current_user.get_progress(), modules=modules)
 
 class LearnController():
@@ -88,56 +87,41 @@ class QuizController():
             completed = True
 
             # populate the form with the user's answers
-            user_answers = [prev_attempt.q1, prev_attempt.q2, prev_attempt.q3, prev_attempt.q4]
+            user_answers = prev_attempt.get_answers()
             QuizController.populate_form(form, user_answers)
 
             # for debugging
             print(prev_attempt.q1, prev_attempt.q2, prev_attempt.q3, prev_attempt.q4)
 
-            # get the quiz solution
-            solution = AlphabetQuiz.query.filter_by(id=1).first()
-
             # construct object to be used to display the correct answers
             sol = AlphabetQuiz.query.filter_by(id=1).first()
-            solutions = [sol.q1, sol.q2, sol.q3, sol.q4]
+            solutions = sol.get_answers()
             results = QuizController.construct_solution(form, solutions)
 
         elif form.validate_on_submit():
             # get the user's submitted answers
-            answer1 = form.question1.data
-            answer2 = int(form.question2.data)
-            answer3 = form.question3.data
-            answer4 = form.question4.data
-
-            # convert checkbox selections into a binary string
-            bin_ans3 = "".join(["1" if str(i) in answer3 else "0" for i in range(4)])
-            bin_ans4 = "".join(["1" if str(i) in answer4 else "0" for i in range(4)])
+            user_answers = QuizController.retrieve_answers(form)
 
             # get the quiz solution
             sol = AlphabetQuiz.query.filter_by(id=1).first()
             
             # calculate the user's score
-            score = 0
-            score = score + 1 if answer1 == sol.q1 else score
-            score = score + 1 if answer2 == sol.q2 else score
-            score += sum([0.25 if bin_ans3[i] == sol.q3[i] else 0 for i in range(4)])
-            score += sum([0.25 if bin_ans4[i] == sol.q4[i] else 0 for i in range(4)])
-            score /= 4
+            score = QuizController.calculate_quiz_score(user_answers, sol.get_answers())
 
-            # for debugging only
-            ans1_correct = answer1 == sol.q1
-            ans2_correct = answer2 == sol.q2
-            ans3_correct = bin_ans3 == sol.q3
-            ans4_correct = bin_ans4 == sol.q4
+            # # for debugging only
+            # ans1_correct = answer1 == sol.q1
+            # ans2_correct = answer2 == sol.q2
+            # ans3_correct = bin_ans3 == sol.q3
+            # ans4_correct = bin_ans4 == sol.q4
 
-            print(answer1, answer2, bin_ans3, bin_ans4)
-            print(sol.q1, sol.q2, sol.q3, sol.q4)
-            # print(ans1_correct, ans2_correct, ans3_correct, ans4_correct)
-            print(score)
-            # debugging end
+            # print(answer1, answer2, bin_ans3, bin_ans4)
+            # print(sol.q1, sol.q2, sol.q3, sol.q4)
+            # # print(ans1_correct, ans2_correct, ans3_correct, ans4_correct)
+            # print(score)
+            # # debugging end
 
             # add the user's attempt to the database
-            attempt = AlphabetQuiz(testee_id=current_user.id, q1=answer1, q2=answer2, q3=bin_ans3, q4=bin_ans4, score=score)
+            attempt = AlphabetQuiz(testee_id=current_user.id, q1=user_answers["answer1"]["value"], q2=user_answers["answer2"]["value"], q3=user_answers["answer3"]["value"], q4=user_answers["answer4"]["value"], score=score)
             db.session.add(attempt)
             db.session.commit()
         return render_template("quiz/quiz_template.html", title="Quiz - Alphabet", heading="Alphabet Quiz", form=form, completed=completed, results=results)
@@ -151,7 +135,7 @@ class QuizController():
             completed = True
 
             # populate the form with the user's answers
-            user_answers = [prev_attempt.q1, prev_attempt.q2, prev_attempt.q3, prev_attempt.q4]
+            user_answers = prev_attempt.get_answers()
             QuizController.populate_form(form, user_answers)
 
             # for debugging
@@ -159,49 +143,40 @@ class QuizController():
 
             # get the quiz solution
             sol =  NumbersQuiz.query.filter_by(id=1).first()
-            solutions = [sol.q1, sol.q2, sol.q3, sol.q4]
+            solutions = sol.get_answers()
             results = QuizController.construct_solution(form, solutions)
 
         elif form.validate_on_submit():
             # get the user's submitted answers
-            # strip white space from string input
-            answer1 = int(form.question1.data)
-            answer2 = form.question2.data.strip()
-            answer3 = int(form.question3.data)
-            answer4 = form.question4.data.strip()
+            user_answers = QuizController.retrieve_answers(form)
 
             # get the quiz solution
             sol = NumbersQuiz.query.filter_by(id=1).first()
 
             # calculate the user's score
-            score = 0
-            score = score + 1 if answer1 == sol.q1 else score
-            score = score + 1 if answer2.lower() == sol.q2 else score
-            score = score + 1 if answer3 == sol.q3 else score
-            score = score + 1 if answer4.lower() == sol.q4 else score
-            score /= 4
+            score = QuizController.calculate_quiz_score(user_answers, sol.get_answers())
 
-            # for debugging only
-            ans1_correct = answer1 == sol.q1
-            ans2_correct = answer2.lower() == sol.q2
-            ans3_correct = answer3 == sol.q3
-            ans4_correct = answer4.lower() == sol.q4
+            # # for debugging only
+            # ans1_correct = answer1 == sol.q1
+            # ans2_correct = answer2.lower() == sol.q2
+            # ans3_correct = answer3 == sol.q3
+            # ans4_correct = answer4.lower() == sol.q4
 
-            print(answer1, answer2, answer3, answer4)
-            print(sol.q1, sol.q2, sol.q3, sol.q4)
-            # print(ans1_correct, ans2_correct, ans3_correct, ans4_correct)
-            print(score)
-            # debugging end
+            # print(answer1, answer2, answer3, answer4)
+            # print(sol.q1, sol.q2, sol.q3, sol.q4)
+            # # print(ans1_correct, ans2_correct, ans3_correct, ans4_correct)
+            # print(score)
+            # # debugging end
 
             # add the user's attempt to the database
-            attempt = NumbersQuiz(testee_id=current_user.id, q1=answer1, q2=answer2, q3=answer3, q4=answer4, score=score)
+            attempt = NumbersQuiz(testee_id=current_user.id,q1=user_answers["answer1"]["value"], q2=user_answers["answer2"]["value"], q3=user_answers["answer3"]["value"], q4=user_answers["answer4"]["value"], score=score)
             db.session.add(attempt)
             db.session.commit()
         return render_template('quiz/quiz_template.html', title="Quiz - Numbers", heading="Numbers Quiz", form=form, completed=completed, results=results)
 
     def populate_form(form, user_answers):
         """
-        helper function that populates a quiz form with the user's answers
+        Helper function that populates a quiz form with the user's answers.
         """
         for question, answer in zip(form, user_answers):
             if question.type == "StringField":
@@ -215,7 +190,7 @@ class QuizController():
 
     def construct_solution(form, solutions):
         """
-        helper function which constructs the results dictionary passed to the html page
+        Helper function which constructs the results dictionary passed to the html page.
         """
         results = {}
         for solution, question, i in zip(solutions, form, range(len(solutions))):
@@ -231,6 +206,52 @@ class QuizController():
                 results[field]["is_correct"] = int(question.data) == solution
                 results[field]["solution"] = solution
             elif question.type == "MultiCheckboxField":
-                user_answer = "".join(["1" if str(i) in question.data else "0" for i in range(4)])
+                user_answer = "".join(["1" if str(i) in question.data else "0" for i in range(len(solution))])
                 results[field]["solution"] = [True if user_answer[i] == solution[i] else False for i in range(len(solution))]
         return results
+
+    def retrieve_answers(form):
+        """
+        Helper function which retrieves the user's answers from a form and puts them into a dictionary.
+        Each entry of the dictionary contains a type (corresponding to the form field type for that question)
+        and a value (the answer itself.)
+        """
+        answers = {}
+        i = 0
+        for question in form:
+            field = "answer"+str(i+1)
+            answers[field] = {}
+            if question.type == "StringField":
+                answers[field]["type"] = "StringField"
+                answers[field]["value"] = question.data.strip()
+                i += 1
+            elif question.type == "RadioField":
+                answers[field]["type"] = "RadioField"
+                answers[field]["value"] = int(question.data)
+                i += 1
+            elif question.type == "IntegerField":
+                answers[field]["type"] = "IntegerField"
+                answers[field]["value"] = question.data
+                i += 1
+            elif question.type == "MultiCheckboxField":
+                answers[field]["type"] = "MultiCheckboxField"
+                answers[field]["value"] = "".join(["1" if str(i) in question.data else "0" for i in range(len(question.choices))])
+                i += 1
+        return answers
+    
+    def calculate_quiz_score(user_answers, solutions):
+        """
+        Helper function that calculates a user's score for a given quiz.
+        """
+        score = 0
+        for answer, solution in zip(user_answers, solutions):
+            if user_answers[answer]["type"] == "StringField" and user_answers[answer]["value"].lower() == solution:
+                score +=1
+            elif user_answers[answer]["type"] == "RadioField" and user_answers[answer]["value"] == solution:
+                score +=1
+            elif user_answers[answer]["type"] == "IntegerField" and user_answers[answer]["value"] == solution:
+                score +=1
+            elif user_answers[answer]["type"] == "MultiCheckboxField":
+                score += sum([0.25 if user_answers[answer]["value"][i] == solution[i] else 0 for i in range(len(solution))])
+        score /= len(solutions)
+        return score
