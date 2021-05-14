620070b8c192c7c23af729819df2b71f0be4ac28
ge75ceb <ge75ceb@mytum.de>
2021-05-14 22:32:22 +0800

    add template w/ graph for usage stats page

--- learn_italian_flask/routes.py
+++ learn_italian_flask/routes.py
@@ -6,13 +6,35 @@ from flask_login import current_user, login_user, logout_user, login_required
 from learn_italian_flask.models import User
 
 from learn_italian_flask.forms import AlphabetQuizForm, NumbersQuizForm, QuizForm
-from learn_italian_flask.models import AlphabetQuiz, NumbersQuiz, Quiz
+from learn_italian_flask.models import AlphabetQuiz, NumbersQuiz, Quiz, UserAnswer
 from learn_italian_flask.controllers import UserController, QuizController, LearnController, DashboardController, ResultsController
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title="Learn Italian")
+
+@app.route('/statistics')
+def statistics():
+    all_user_quizzes = UserAnswer.query.all()
+    num_quizzes_completed = sum([1 for quiz in all_user_quizzes])
+    print(num_quizzes_completed)
+    all_quizzes = Quiz.query.all()
+
+    labels = []
+    if all_quizzes is not None:
+        labels = [quiz.name for quiz in all_quizzes]
+    cumulative_scores = [0 for i in range(len(labels))]
+    attempts = [0 for i in range(len(labels))]
+    for quiz in all_user_quizzes:
+        cumulative_scores[quiz.quiz_id-1] += quiz.score * 100
+        attempts[quiz.quiz_id-1] += 1
+    scores = [round(score/attempt) if attempt != 0 else 0 for score, attempt in zip(cumulative_scores, attempts)]
+
+    users = User.query.all()
+    num_users = sum([1 for quiz in all_quizzes])
+    print(num_users)
+    return render_template('statistics.html', title="Learn Italian — Usage Statistics", labels=labels, scores=scores)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
